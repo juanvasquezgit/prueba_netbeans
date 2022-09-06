d9fd1d3f649c8f6d63382ce3817220f195708a63
juan esteban <juan.vasquez03@uceva.edu.co>
2022-09-06 18:16:17 -0500

    EJEMPLO GIT

---
+++ arquitectura de computadores/New Folder/example01_imprimir/example02.s
@@ -0,0 +1,52 @@
+/* este es un comentario multilinea*/
+//comentario de una linea
+#comentario de una linie tipo ensamblador
+#.dta es el segmento de memoria donde se encuentran las variables
+# : es igual a tequeta 
+.data
+mensaje1:
+mensaje2:
+mensaje1 : .ascii "esta es una impresion desde ASSEMBLER\xa\xd\x0" 
+mensaje2 : .ascii "este es otro string\0"
+numero: .float 2.345
+.text
+imprimir_asm:
+    mov $2 %rax  #RAX =2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ arquitectura de computadores/New Folder/example01_imprimir/main.c
@@ -0,0 +1,55 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2022 Usuario.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Juan vasquez <juanesteban.vasquez@hotmail.com>
+ *
+ * Created on 2 de septiembre de 2022, 10:56 a. m.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+void imprimir();
+
+int main(int argc, char** argv) {
+    imprimir();
+    void imprimir_asm();
+    return (EXIT_SUCCESS);
+}
+/**
+ * funcion imprime desde lenguaje c 
+ * @param a es un entero q ....
+ * @param nombre es un string que ......
+ */
+void imprimir(){
+    printf("ya va a funcionar\r\n"); 
+}
+
+
